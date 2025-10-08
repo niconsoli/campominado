c959c35fa5a84b5395f47915301c84a8bb6e2af8
nicolas consoli <p4059149@gmail.com>
2025-10-08 17:39:12 -0300

    Update Minesweeper.java
    while permitindo loop do programa!

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -5,33 +5,82 @@ public class Minesweeper {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in); Random random = new Random();
         int opt; String jogada; String[] jogadaArray; String acao = ""; int linha = 0; int coluna = 0;
-        System.out.println("Bem-vindo ao Campo Minado!");
-        System.out.println("1. Fácil (8x8)");
-        System.out.println("2. Intermediário (16x16)");
-        System.out.println("3. Difícil (32x32)");
-        System.out.print("Selecione a dificuldade que deseja jogar: ");
-        opt = sc.nextInt(); sc.nextLine(); //segundo nextLine é um debug p linha 31
+        while (true) { 
+            System.out.println();
+            System.out.println("Bem-vindo ao Campo Minado!");
+            System.out.println("1. Fácil (8x8)");
+            System.out.println("2. Intermediário (16x16)");
+            System.out.println("3. Difícil (32x32)");
+            System.out.println("0. Sair");
+            System.out.print("Selecione a dificuldade que deseja jogar: ");
+            opt = sc.nextInt(); sc.nextLine(); //segundo nextLine é um debug p linha 31
 
-        switch (opt){
-            case 1:
-                Tabela tab = new Tabela(8,8);
-                tab.imprimir();
-                System.out.println("Todas as jogadas devem seguir o padrão abaixo:");
-                System.out.println("(ação) (linha) (coluna)");
-                System.out.println("Exemplos:");
-                System.out.println("reveal 1 3 (revela a célula 1x3), flag 3 2 (bandeira a célula 3x2), reveal 4 4 (revela a célula 4x4)");
-                System.out.println("Usar flag em uma região que já tem bandeira tirará a bandeira dali.");
-                tab.colocarBombas();
-                tab.calcularBombasArd();
-                while (!tab.endgame && !tab.gameWin){
-                    System.out.print("Digite sua jogada a seguir: ");
-                    jogada = sc.nextLine();
-                    jogadaArray = jogada.split(" ");
-                    if (jogadaArray.length == 3){
-                    acao = jogadaArray[0];
-                    linha = Integer.parseInt(jogadaArray[1]) - 1;
-                    coluna = Integer.parseInt(jogadaArray[2]) - 1;}
-                    if (linha >= 0 && linha < tab.tabela.length && coluna >= 0 && coluna < tab.tabela[0].length){
+            if (opt == 0){
+                System.out.println("Certo, encerrando o sistema agora...");
+                break;
+            }
+            
+            switch (opt){
+                case 1:
+                    Tabela tab = new Tabela(8,8);
+                    tab.imprimir();
+                    System.out.println("Todas as jogadas devem seguir o padrão abaixo:");
+                    System.out.println("(ação) (linha) (coluna)");
+                    System.out.println("Exemplos:");
+                    System.out.println("reveal 1 3 (revela a célula 1x3), flag 3 2 (bandeira a célula 3x2), reveal 4 4 (revela a célula 4x4)");
+                    System.out.println("Usar flag em uma região que já tem bandeira tirará a bandeira dali.");
+                    tab.colocarBombas();
+                    tab.calcularBombasArd();
+                    while (!tab.endgame && !tab.gameWin){
+                        System.out.print("Digite sua jogada a seguir: ");
+                        jogada = sc.nextLine();
+                        jogadaArray = jogada.split(" ");
+                        if (jogadaArray.length == 3){
+                        acao = jogadaArray[0];
+                        linha = Integer.parseInt(jogadaArray[1]) - 1;
+                        coluna = Integer.parseInt(jogadaArray[2]) - 1;}
+                        if (linha >= 0 && linha < tab.tabela.length && coluna >= 0 && coluna < tab.tabela[0].length){
+                            switch (acao){
+                                case "reveal":
+                                tab.reveal(linha, coluna);
+                                break;
+                                case "flag":
+                                tab.toggleFlag(linha, coluna);
+                                break;
+                                case "rvtd":
+                                tab.revelarTudo();
+                                break;
+                                default:
+                                System.out.print("Ação inválida, tente novamente."); System.out.println();
+                                break;}}
+                        tab.checarVitoria();
+                        tab.imprimir();}
+                    if (tab.endgame){
+                        System.out.println();
+                        tab.revelarTudo();
+                        tab.imprimir();
+                        System.out.println("Você perdeu!");} 
+                    if (tab.gameWin){
+                        System.out.println("Parabéns, você venceu!");}
+                break;
+                case 2:
+                    tab = new Tabela(16,16);
+                    tab.imprimir();
+                    System.out.println("Todas as jogadas devem seguir o padrão abaixo:");
+                    System.out.println("(ação) (linha) (coluna)");
+                    System.out.println("Exemplos:");
+                    System.out.println("reveal 1 3 (revela a célula 1x3), flag 3 2 (bandeira a célula 3x2), reveal 4 4 (revela a célula 4x4)");
+                    System.out.println("Usar flag em uma região que já tem bandeira tirará a bandeira dali.");
+                    tab.colocarBombas();
+                    tab.calcularBombasArd();
+                    while (!tab.endgame && !tab.gameWin){
+                        System.out.print("Digite sua jogada a seguir: ");
+                        jogada = sc.nextLine();
+                        jogadaArray = jogada.split(" ");
+                        acao = jogadaArray[0];
+                        linha = Integer.parseInt(jogadaArray[1]) - 1;
+                        coluna = Integer.parseInt(jogadaArray[2]) - 1;
+                        if (linha >= 0 && linha < tab.tabela.length && coluna >= 0 && coluna < tab.tabela[0].length){
                         switch (acao){
                             case "reveal":
                             tab.reveal(linha, coluna);
@@ -39,100 +88,60 @@ public class Minesweeper {
                             case "flag":
                             tab.toggleFlag(linha, coluna);
                             break;
-                            case "rvtd":
-                            tab.revelarTudo();
+                            default:
+                            System.out.print("Ação inválida, tente novamente."); System.out.println();
+                            break;}}
+                        tab.checarVitoria();
+                        tab.imprimir();}
+                    if (tab.endgame){
+                        System.out.println();
+                        tab.revelarTudo();
+                        tab.imprimir();
+                        System.out.println("Você perdeu!");} 
+                    if (tab.gameWin){
+                        System.out.println("Parabéns, você venceu!");}
+                break;
+                case 3:
+                    tab = new Tabela(32,32);
+                    tab.imprimir();
+                    System.out.println("Todas as jogadas devem seguir o padrão abaixo:");
+                    System.out.println("(ação) (linha) (coluna)");
+                    System.out.println("Exemplos:");
+                    System.out.println("reveal 1 3 (revela a célula 1x3), flag 3 2 (bandeira a célula 3x2), reveal 4 4 (revela a célula 4x4)");
+                    System.out.println("Usar flag em uma região que já tem bandeira tirará a bandeira dali.");
+                    tab.colocarBombas();
+                    tab.calcularBombasArd();
+                    while (!tab.endgame && !tab.gameWin){
+                        System.out.print("Digite sua jogada a seguir: ");
+                        jogada = sc.nextLine();
+                        jogadaArray = jogada.split(" ");
+                        acao = jogadaArray[0];
+                        linha = Integer.parseInt(jogadaArray[1]) - 1;
+                        coluna = Integer.parseInt(jogadaArray[2]) - 1;
+                        if (linha >= 0 && linha < tab.tabela.length && coluna >= 0 && coluna < tab.tabela[0].length){
+                        switch (acao){
+                            case "reveal":
+                            tab.reveal(linha, coluna);
+                            break;
+                            case "flag":
+                            tab.toggleFlag(linha, coluna);
                             break;
                             default:
                             System.out.print("Ação inválida, tente novamente."); System.out.println();
                             break;}}
-                    tab.checarVitoria();
-                    tab.imprimir();}
-                if (tab.endgame){
-                    System.out.println();
-                    tab.revelarTudo();
-                    tab.imprimir();
-                    System.out.println("Você perdeu!");} 
-                if (tab.gameWin){
-                    System.out.println("Parabéns, você venceu!");}
-            break;
-            case 2:
-                tab = new Tabela(16,16);
-                tab.imprimir();
-                System.out.println("Todas as jogadas devem seguir o padrão abaixo:");
-                System.out.println("(ação) (linha) (coluna)");
-                System.out.println("Exemplos:");
-                System.out.println("reveal 1 3 (revela a célula 1x3), flag 3 2 (bandeira a célula 3x2), reveal 4 4 (revela a célula 4x4)");
-                System.out.println("Usar flag em uma região que já tem bandeira tirará a bandeira dali.");
-                tab.colocarBombas();
-                tab.calcularBombasArd();
-                while (!tab.endgame && !tab.gameWin){
-                    System.out.print("Digite sua jogada a seguir: ");
-                    jogada = sc.nextLine();
-                    jogadaArray = jogada.split(" ");
-                    acao = jogadaArray[0];
-                    linha = Integer.parseInt(jogadaArray[1]) - 1;
-                    coluna = Integer.parseInt(jogadaArray[2]) - 1;
-                    if (linha >= 0 && linha < tab.tabela.length && coluna >= 0 && coluna < tab.tabela[0].length){
-                    switch (acao){
-                        case "reveal":
-                        tab.reveal(linha, coluna);
-                        break;
-                        case "flag":
-                        tab.toggleFlag(linha, coluna);
-                        break;
-                        default:
-                        System.out.print("Ação inválida, tente novamente."); System.out.println();
-                        break;}}
-                    tab.checarVitoria();
-                    tab.imprimir();}
-                if (tab.endgame){
-                    System.out.println();
-                    tab.revelarTudo();
-                    tab.imprimir();
-                    System.out.println("Você perdeu!");} 
-                if (tab.gameWin){
-                    System.out.println("Parabéns, você venceu!");}
-            break;
-            case 3:
-                tab = new Tabela(32,32);
-                tab.imprimir();
-                System.out.println("Todas as jogadas devem seguir o padrão abaixo:");
-                System.out.println("(ação) (linha) (coluna)");
-                System.out.println("Exemplos:");
-                System.out.println("reveal 1 3 (revela a célula 1x3), flag 3 2 (bandeira a célula 3x2), reveal 4 4 (revela a célula 4x4)");
-                System.out.println("Usar flag em uma região que já tem bandeira tirará a bandeira dali.");
-                tab.colocarBombas();
-                tab.calcularBombasArd();
-                while (!tab.endgame && !tab.gameWin){
-                    System.out.print("Digite sua jogada a seguir: ");
-                    jogada = sc.nextLine();
-                    jogadaArray = jogada.split(" ");
-                    acao = jogadaArray[0];
-                    linha = Integer.parseInt(jogadaArray[1]) - 1;
-                    coluna = Integer.parseInt(jogadaArray[2]) - 1;
-                    if (linha >= 0 && linha < tab.tabela.length && coluna >= 0 && coluna < tab.tabela[0].length){
-                    switch (acao){
-                        case "reveal":
-                        tab.reveal(linha, coluna);
-                        break;
-                        case "flag":
-                        tab.toggleFlag(linha, coluna);
-                        break;
-                        default:
-                        System.out.print("Ação inválida, tente novamente."); System.out.println();
-                        break;}}
-                    tab.checarVitoria();
-                    tab.imprimir();}
-                if (tab.endgame){
-                    System.out.println();
-                    tab.revelarTudo();
-                    tab.imprimir();
-                    System.out.println("Você perdeu!");} 
-                if (tab.gameWin){
-                    System.out.println("Parabéns, você venceu!");}
-            break;
-            default:
-            System.out.println("Valor inserido incorretamente. Reiniciando sistema...");
+                        tab.checarVitoria();
+                        tab.imprimir();}
+                    if (tab.endgame){
+                        System.out.println();
+                        tab.revelarTudo();
+                        tab.imprimir();
+                        System.out.println("Você perdeu!");} 
+                    if (tab.gameWin){
+                        System.out.println("Parabéns, você venceu!");}
+                break;
+                default:
+                System.out.println("Valor inserido incorretamente. Reiniciando sistema...");
+            }
         }
     }
 }
